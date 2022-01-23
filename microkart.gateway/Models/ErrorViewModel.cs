6b744910be85f6aac5fcee514e81183c1eae9912
Gopal Kumar Thakur <send2gopal@gmail.com>
2022-01-22 23:58:34 -0500

    Added ocelot  gateway

---
+++ microkart.gateway/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace microkart.gateway.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
