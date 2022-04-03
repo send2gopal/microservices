ed9e65f3d4c5e2ac9662d5fdb1bf29891d5a1b18
Gopal Kumar Thakur <send2gopal@gmail.com>
2022-04-03 12:05:50 -0400

    Added Frontend

---
+++ microkart.frontend/src/app/home/home.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+import { HomeRoutingModule } from './home-routing.module';
+
+
+import { ElectronicsComponent } from './electronics/electronics.component';
+
+// Widgest Components
+import { SliderComponent } from './widgets/slider/slider.component';
+import { BlogComponent } from './widgets/blog/blog.component';
+import { LogoComponent } from './widgets/logo/logo.component';
+import { ServicesComponent } from './widgets/services/services.component';
+import { CollectionComponent } from './widgets/collection/collection.component';
+
+@NgModule({
+  declarations: [
+
+    ElectronicsComponent,
+
+    // Widgest Components
+    SliderComponent,
+    BlogComponent,
+    LogoComponent,
+    ServicesComponent,
+    CollectionComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    SharedModule
+  ]
+})
+export class HomeModule { }
